b3b779e61cdb9d6b0dc481bd449049e326198ed5
Matthias Brantner <matthias.brantner@oracle.com>
2016-10-07 20:42:05 -0700

    simplify navbar

--- src/app/components/navbar/navbar.directive.spec.js
+++ src/app/components/navbar/navbar.directive.spec.js
@@ -2,44 +2,8 @@
   'use strict';
 
   /**
-   * @todo Complete the test
-   * This example is not perfect.
-   * Test should check if MomentJS have been called
    */
   describe('directive navbar', function() {
-    // var $window;
-    var vm;
-    var el;
-    var timeInMs;
 
-    beforeEach(module('hellobible'));
-    beforeEach(inject(function($compile, $rootScope) {
-      // spyOn(_$window_, 'moment').and.callThrough();
-      // $window = _$window_;
-
-      timeInMs = new Date();
-      timeInMs = timeInMs.setHours(timeInMs.getHours() - 24);
-
-      el = angular.element('<acme-navbar creation-date="' + timeInMs + '"></acme-navbar>');
-
-      $compile(el)($rootScope.$new());
-      $rootScope.$digest();
-      vm = el.isolateScope().vm;
-      // ctrl = el.controller('acmeNavbar');
-    }));
-
-    it('should be compiled', function() {
-      expect(el.html()).not.toEqual(null);
-    });
-
-    it('should have isolate scope object with instanciate members', function() {
-      expect(vm).toEqual(jasmine.any(Object));
-
-    });
-
-    // it('should call Moment', function() {
-    //   console.log($window.moment)
-    //   expect($window.moment).toHaveBeenCalled();
-    // });
   });
 })();
