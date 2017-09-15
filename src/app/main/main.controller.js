d55ca441f62f7c3fdbc58ba68f74daae7686cd66
Cornelia Brantner <conny.brantner@gmail.com>
2017-09-14 19:36:15 -0700

    why and what

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -45,30 +45,85 @@
     vm.how = [
       {
         image : "/assets/images/how-image1",
-        text : "<b>Choose your subscription</b> and pick the number of kids.",
-        button : "Try It Now",
-        link : "#pricing"
+        text : "<b>EXPLORE</b> Bible-based and Christ-centered learning.",
+        quote : "I loved it! I like that Jesus is tied into every story. This is a great way to point children to The Savior.",
+        name : "Bethany",
+        title : "Sunday school teacher and mom of 2"
       },
       {
         image : "/assets/images/how-image2",
-        text : "<b>Receive a box</b> every month and find creative, fun, and hands-on Bible lessons.",
-        button : "Explore Past Boxes",
-        link : "/blog/categories/past-boxes"
+        text : "<b>ENJOY</b> child-friendly instructions and thoughful teaching guides.",
+        quote : "The unique crafts also come with clear, attractive directions for assembly (which makes my teacher heart go pitter-patter). Noah’s Ark box also had thoughtful faith formation opportunities and discussion questions.",
+        name : "Maria",
+        title : "Kindergarten teacher and founder of <a href='http://kindercraze.com/meet-hellobible-christian-subscription-box-kids/'>Kinder Craze</a>"
       },
       {
         image : "/assets/images/how-image3",
-        text : "<b>Start your journey</b> in Sunday school, homeschool, or within your Family.",
-        button : "What Others Say",
-        link : "#reviews"
+        text : "<b>CREATE</b> hands-on crafts and fun activities.",
+        quote : "Very appealing, high-quality materials and keepsake crafts. My 7 year old daughter really liked the story book and the fun mix of activities.",
+        name : "Tenny",
+        title : "Homeschooling mom of 3"
+      },
+      {
+        image : "/assets/images/how-image3",
+        text : "It is <b>CONVENIENT</b>. All Materials are included and delivered.",
+        quote : "I like that everything is there, I don’t have to go to the store. We are busy all week, it is great to have the pre-made box that puts it all together.",
+        name : "Sarah",
+        title : "Working mom of 2"
       }
     ];
 
     vm.options = [
       {
-        id : "premium",
-        name : "Premium Box",
+        id : "families",
+        name : "Families",
+        titleImage : "/assets/images/info/envelope.jpg",
+        carousel : [
+          {
+            image : "envelope-content",
+            title : "Crafts, instructions and ideas to share &amp; pray."
+          },
+          {
+            image : "read",
+            title : "Read a wonderful Bible story together."
+          },
+          {
+            image : "play",
+            title : "Play an awesome matching game. Help Noah to bring two of all the animals into the Ark."
+          },
+          {
+            image : "testimonial",
+            title : "What our customers say."
+          },
+          {
+            image : "easter_pack",
+            title : "Content of the Easter Pack."
+          }
+        ],
+        title : "You get one HelloBible Box/month. Great for Kids Ages 5+.",
+        subtitle: "Ready to start Your Journey?",
+        bullets: [
+          {
+            title : "Bible Storybook.",
+            text : "Read and enjoy one Bible Story each month."
+          },
+          {
+            title : "Parent Guides",
+            text : "Thoughtful guides and fun family activities to dig deeper into the story."
+          },
+          {
+            title : "Creative Projects",
+            text : "Story-focused keepsake crafts. High-quality material and attractive instructions for assembly."
+          },
+          {
+            title : "Kids Bible Journal",
+            text : "Story coloring, activity sheets and memory verses."
+          }
+        ]
+      }, {
+        id : "schools",
+        name : "Sunday Schools & Homeschooling",
         titleImage : "/assets/images/info/premium.jpg",
-        ribbon : "Full Curriculum",
         carousel : [
           {
             image : "premium-content",
@@ -91,10 +146,26 @@
             title : "Content of the Easter Box."
           }
         ],
-        title : "HelloBible Premium Box",
-        text : '<p><span class="text-read">Read</span> an age-appropriate Bible Story Book and <span class="text-create">create</span> two wonderful crafts to deepen the understanding of the Bible story. Enjoy high quality family time. <span class="text-pray">Share and pray</span> ' +
-              'together.' +
-                            '</p>'
+        title : "You Get a Fun Curriculum for Grades K-3",
+        subtitle: "Ready to start the Journey?",
+        bullets: [
+          {
+            title : "Bible Storybook",
+            text : "Read and enjoy a Bible Story."
+          },
+          {
+            title : "Lesson Guides",
+            text : "Thoughtful lesson guides and fun activities to dig deeper into the story."
+          },
+          {
+            title : "Creative Projects",
+            text : "Story-focused keepsake crafts. High-quality material and attractive instructions for assembly."
+          },
+          {
+            title : "Kids Bible Journal",
+            text : "Story coloring, activity sheets and memory verses."
+          }
+        ]
       }
     ];
 
