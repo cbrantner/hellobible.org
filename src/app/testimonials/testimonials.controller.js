b6a02e4174917bddf546e1313ee044149eb8c047
Cornelia Brantner <conny.brantner@gmail.com>
2017-11-17 22:22:06 -0800

    simplify mobile site

---
+++ src/app/testimonials/testimonials.controller.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('hellobible')
+    .controller('TestimonialsController', TestimonialsController);
+
+
+  /** @ngInject */
+  function TestimonialsController() {
+  }
+
+})();
