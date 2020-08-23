c33b50094d6e5ca9b4023150ff82e4e935da3883
Cornelia Brantner <conny.brantner@gmail.com>
2020-08-23 16:25:23 -0700

    add peek inside for junior

---
+++ src/app/noah-junior/noah-junior.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-noah-junior',
+  templateUrl: './noah-junior.component.html',
+  styleUrls: ['./noah-junior.component.scss']
+})
+export class NoahJuniorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
